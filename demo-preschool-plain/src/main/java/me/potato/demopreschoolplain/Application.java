45525bc4427fce6af8349f5c524583d6f73b9b78
Potato White <bravopotato@gmail.com>
2019-09-16 20:21:38 +0900

    demo

---
+++ demo-preschool-plain/src/main/java/me/potato/demopreschoolplain/Application.java
@@ -0,0 +1,13 @@
+package me.potato.demopreschoolplain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
